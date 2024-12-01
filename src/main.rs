c1f0a08b63b28f1eb534853a69fde4ff490e5751
daizyoo <daiki20070809@icloud.com>
2024-12-01 17:42:46 +0900

    [update] add `sqlx` crate. [update] change test.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod room;
 mod types;
 mod user;
 
-use std::{collections::HashMap, sync::Mutex};
+use std::{collections::HashMap, env, sync::Mutex};
 
 use actix_cors::Cors;
 use actix_web::{
@@ -13,6 +13,7 @@ use actix_web::{
     App, HttpServer,
 };
 
+use sqlx::MySqlPool;
 use tracing_subscriber::EnvFilter;
 
 use types::{Message, Room, RoomId, User, UserId, UserInfo};
@@ -86,48 +87,26 @@ impl RoomList {
     }
 }
 
+struct Database {
+    pool: MySqlPool,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().expect("Failed to load .env file");
 
+    let pool = MySqlPool::connect(&env::var("DATABASE_URL")?).await?;
+    let pool = Data::new(Mutex::new(Database { pool }));
+
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
-
-    let _user = vec![
-        User::new(
-            "daiki".to_string(),
-            "@daiki".to_string(),
-            "daiki".to_string(),
-        ),
-        User::new(
-            "kouta".to_string(),
-            "@kouta".to_string(),
-            "kouta".to_string(),
-        ),
-    ];
-
-    // let rooms = Data::new(Mutex::new(RoomList(vec![Room::new(
-    //     1,
-    //     vec![UserInfo::from(&user[0]), UserInfo::from(&user[1])],
-    // )])));
-    // let messages = Data::new(Mutex::new(MessageList(HashMap::from([(1, Vec::new())]))));
-    // let friends = Data::new(Mutex::new(FriendList(HashMap::from([
-    //     (String::from(user[0].id()), vec![UserInfo::from(&user[1])]),
-    //     (String::from(user[1].id()), vec![UserInfo::from(&user[0])]),
-    // ]))));
-    // let users = Data::new(Mutex::new(UserList(user)));
-    let rooms = Data::new(Mutex::new(RoomList(Vec::new())));
-    let messages = Data::new(Mutex::new(MessageList(HashMap::new())));
-    let friends = Data::new(Mutex::new(FriendList(HashMap::new())));
-    let users = Data::new(Mutex::new(UserList(Vec::new())));
 
     HttpServer::new(move || {
         App::new()
             .wrap(
                 Cors::default()
                     .allowed_origin("http://localhost:3000")
-                    .allowed_origin("https://c3d9-42-125-172-148.ngrok-free.app")
                     .allowed_methods(vec!["GET", "POST"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
@@ -156,10 +135,7 @@ async fn main() -> anyhow::Result<()> {
                     .route("/search", post().to(friend::get))
                     .route("/delete", post().to(friend::delete)),
             )
-            .app_data(rooms.clone())
-            .app_data(messages.clone())
-            .app_data(friends.clone())
-            .app_data(users.clone())
+            .app_data(pool.clone())
     })
     .bind(("127.0.0.1", 3478))?
     .run()
@@ -170,118 +146,21 @@ async fn main() -> anyhow::Result<()> {
 
 #[cfg(test)]
 mod test {
-    use crate::*;
-    use actix_web::{body::MessageBody, test, web::Json, HttpResponse};
-    use types::{GetMessages, LoginInfo, MessageInfo};
+    use std::env;
 
-    type DataM<T> = Data<Mutex<T>>;
+    use anyhow::Result;
 
-    fn data_mutex<T>(data: T) -> DataM<T> {
-        Data::new(Mutex::new(data))
-    }
+    use sqlx::MySqlPool;
 
-    fn message_list<const N: usize>(map: [(u32, Vec<Message>); N]) -> DataM<MessageList> {
-        data_mutex(MessageList(HashMap::from(map)))
-    }
-    fn room_list(vec: Vec<Room>) -> DataM<RoomList> {
-        data_mutex(RoomList(vec))
-    }
-    fn friend_list<const N: usize>(map: [(UserId, Vec<UserInfo>); N]) -> DataM<FriendList> {
-        data_mutex(FriendList(HashMap::from(map)))
-    }
-    fn user_list(vec: Vec<User>) -> DataM<UserList> {
-        data_mutex(UserList(vec))
+    pub async fn connect() -> Result<MySqlPool> {
+        dotenvy::dotenv()?;
+        Ok(MySqlPool::connect(&env::var("DATABASE_URL")?).await?)
     }
 
-    fn user(name: &str, id: &str, pass: &str) -> User {
-        User::new(name.to_string(), id.to_string(), pass.to_string())
+    #[tokio::test]
+    async fn mysql_connect() -> anyhow::Result<()> {
+        let pool = connect().await?;
+        pool.close().await;
+        Ok(())
     }
-    fn info_user(name: &str, id: &str) -> UserInfo {
-        UserInfo::new(name.to_string(), id.to_string())
-    }
-    fn login_user(id: &str, pass: &str) -> LoginInfo {
-        LoginInfo::new(id.to_string(), pass.to_string())
-    }
-
-    fn default_rooms() -> Vec<Room> {
-        vec![Room::new(
-            0,
-            vec![info_user("daiki", "@daiki"), info_user("kouta", "@kouta")],
-        )]
-    }
-
-    fn default_data() -> (
-        DataM<MessageList>,
-        DataM<RoomList>,
-        DataM<UserList>,
-        DataM<FriendList>,
-    ) {
-        (
-            message_list([(0, vec![])]),
-            room_list(default_rooms()),
-            user_list(vec![
-                user("daiki", "@daiki", "daiki"),
-                user("kouta", "@kouta", "kouta"),
-            ]),
-            friend_list([(String::from("@daiki"), vec![])]),
-        )
-    }
-
-    fn body_string(res: HttpResponse) -> String {
-        let bytes = res.into_body().try_into_bytes().unwrap().to_vec();
-        String::from_utf8(bytes).unwrap()
-    }
-    fn print_data<T: std::fmt::Debug>(data: &DataM<T>) -> std::sync::MutexGuard<'_, T> {
-        data.lock().unwrap()
-    }
-
-    #[test]
-    async fn message_get() {
-        let (msgs, rooms, users, _) = default_data();
-        let json = Json(GetMessages::new(
-            0,
-            LoginInfo::new(String::from("@daiki"), String::from("daiki")),
-        ));
-        println!("{:#?}", print_data(&msgs).0);
-        let res = message::get_message(msgs, users, rooms, json).await;
-        let body = body_string(res);
-        println!("{}", body)
-    }
-    #[test]
-    async fn message_send() {
-        let (msgs, _, _, _) = default_data();
-        let msgsc = msgs.clone();
-        let res = message::send_message(
-            msgsc,
-            Json(MessageInfo::new(
-                0,
-                String::from("test message"),
-                LoginInfo::new("@daiki".to_string(), "daiki".to_string()),
-            )),
-        )
-        .await;
-
-        let body = body_string(res);
-        println!("{}", body);
-        println!("{:#?}", print_data(&msgs));
-    }
-
-    #[test]
-    async fn room_create() {}
-    #[test]
-    async fn room_get() {}
-
-    #[test]
-    async fn user_create() {}
-    #[test]
-    async fn user_search() {}
-    #[test]
-    async fn user_login() {}
-
-    #[test]
-    async fn friend_add() {}
-    #[test]
-    async fn friend_get() {}
-    #[test]
-    async fn friend_delete() {}
 }
