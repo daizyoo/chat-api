a3f952b9942da76c3e0e3523ff504795bfc1dbe0
daizyoo <daiki20070809@icloud.com>
2024-12-02 13:26:33 +0900

    [update] change friend get function.

--- src/friend/get.rs
+++ src/friend/get.rs
@@ -1,20 +1,10 @@
 use super::*;
 
-pub async fn get(
-    friends: Data<Mutex<FriendList>>,
-    users: Data<Mutex<UserList>>,
-    Json(user): Json<UserInfo>,
-) -> HttpResponse {
-    {
-        let users = users.lock().unwrap();
-        if !users.exist(user.id()) {
-            return Response::error("not found user");
-        }
-    }
-    let friends = friends.lock().unwrap();
-    if let Some(vec) = friends.0.get(user.id()) {
-        Response::ok(vec)
-    } else {
-        Response::error("not found friend list")
-    }
+pub async fn get(db: Data<Database>, Json(user): Json<UserInfo>) -> Result<HttpResponse> {
+    let user = sqlx::query_as!(QueryUser, "SELECT * FROM users WHERE id = ?", user.id())
+        .fetch_one(&db.pool)
+        .await?;
+    let friends: Friends = user.friends.into();
+
+    Ok(Response::ok(friends.list))
 }
